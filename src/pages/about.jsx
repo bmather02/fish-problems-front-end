cc6f18865419d86b27e8a783adb30dcc032ad2c4
Me <104526114+bmather02@users.noreply.github.com>
2022-07-19 19:43:15 -0400

    adds column class to about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,7 @@
 
 function About(props) {
     return (
-        <main className="center">
+        <main className="center column">
             <h1 className="center">About</h1>
             <br/>
             <h6>Fish Problems is a resource to help aquarium enthusiasts find the solutions to their problems. Fish Problems is free to use and is not responsible for any fish deaths or damage.</h6>
