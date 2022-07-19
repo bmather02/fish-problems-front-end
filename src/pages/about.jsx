764f3ebda701215c3b3ae62a0f891159d4d75c3e
Me <104526114+bmather02@users.noreply.github.com>
2022-07-19 16:34:09 -0400

    adds home page and about page content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,10 @@
 
 function About(props) {
     return (
-        <main>
+        <main className="center">
             <h1 className="center">About</h1>
+            <br/>
+            <h6>Fish Problems is a resource to help aquarium enthusiasts find the solutions to their problems. Fish Problems is free to use and is not responsible for any fish deaths or damage.</h6>
         </main>
     )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,14 @@
-
+import {Link} from 'react-router-dom'
 function Home(props) {
     return (
-        <main>
+        <main className="center">
             <h1 className="center">You've Got 99 Problems, But A Fish Ain't One</h1>
+            <br/>
+            <h3>We hope we can help you find the solution to your problem. Check below to see our solution to your problem.</h3>
+            <br/>
+            <Link to="/problems" className="button">
+                <h5>Common Problems</h5>
+            </Link>
         </main>
     )
 }
